ca05ef32aa3cc22c99a852292640308bf56099b6
Terenzio Avantaggiato <budabuda.factoring@gmail.com>
2020-02-04 20:20:56 +0100

    Aggiunto routing di base

--- src/MyCourse/Startup.cs
+++ src/MyCourse/Startup.cs
@@ -16,6 +16,8 @@ namespace MyCourse
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -32,11 +34,10 @@ namespace MyCourse
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+               // per gestire url tipo: /courses/detail/5
+                endpoints.MapControllerRoute("default", "{controller=home}/{action=index}/{id?}");
             });
+
         }
     }
 }
